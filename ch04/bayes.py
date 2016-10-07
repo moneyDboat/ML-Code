fc9f407b8febea360ccfacd8681f2c6c5a7ab492
moneyDboat <moneyboat@foxmail>
2016-10-07 18:09:44 +0800

    complete ch04

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -102,6 +102,86 @@ def spamTest():
         wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
         if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
             errorCount += 1
-            print ("classification error",docList[docIndex])
+            print ("classification error",docList[docIndex],classList[docIndex])
     print ('the error rate is: ',float(errorCount)/len(testSet))
     #return vocabList,fullText
+
+
+def calcMostFreq(vocabList,fullText):
+    import operator
+    freqDict = {}
+    for token in vocabList:
+        freqDict[token]=fullText.count(token)
+    sortedFreq = sorted(freqDict.items(), key=operator.itemgetter(1), reverse=True) 
+    return sortedFreq[:30]       
+
+def getRSS():
+	import feedparser
+	ny=feedparser.parse('http://newyork.craigslist.org/stp/index.rss')
+	sf=feedparser.parse('http://sfbay.craigslist.org/stp/index.rss')
+	return ny, sf
+
+def localWords(feed1,feed0):
+    import feedparser
+    docList=[]; classList = []; fullText =[]
+    minLen = min(len(feed1['entries']),len(feed0['entries']))
+    for i in range(minLen):
+        wordList = textParse(feed1['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1) #NY is class 1
+        wordList = textParse(feed0['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)#create vocabulary
+    top30Words = calcMostFreq(vocabList,fullText)   #remove top 30 words
+    for pairW in top30Words:
+        if pairW[0] in vocabList: vocabList.remove(pairW[0])
+    trainingSet = list(range(2*minLen)); testSet=[]           #create test set
+    for i in range(20):
+        randIndex = int(random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])  
+    trainMat=[]; trainClasses = []
+    for docIndex in trainingSet:#train the classifier (get probs) trainNB0
+        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:        #classify the remaining items
+        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+            errorCount += 1
+    rate = float(errorCount)/len(testSet)
+    print ('the error rate is: ', rate)
+    return rate,vocabList,p0V,p1V
+
+def getTopWords(ny,sf):
+    import operator
+    vocabList,p0V,p1V=localWords(ny,sf)
+    topNY=[]; topSF=[]
+    for i in range(len(p0V)):
+        if p0V[i] > -5.0 : topSF.append((vocabList[i],p0V[i]))
+        if p1V[i] > -5.0 : topNY.append((vocabList[i],p1V[i]))
+    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
+    print ("SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**")
+    for item in sortedSF:
+        print (item[0])
+    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
+    print ("NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**")
+    for item in sortedNY:
+        print (item[0])
+
+def calAver():
+	rates = []
+	#use same input
+	ny,sf = getRSS()
+	for i in range(10):
+		rate,result,p0,p1 = localWords(ny,sf)
+		rates.append(rate)
+	return mean(rates)
+
+
+
+
